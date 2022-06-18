40d08e694fc4a6741e460e42bd139e038243c378
Lucas Chagas <lucas.w.chagas@gmail.com>
2022-06-17 22:10:42 -0400

    Replaced promise chain with async/await

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,20 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
 
-  const fetchMoviesHandler = (e) => {
-    fetch("https://swapi.dev/api/films/")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        setMovies(
-          data.results.reduce((acc, curr) => {
-            const { episode_id, opening_crawl, release_date, ...rest } = curr;
-            acc.push({
-              ...rest,
-              id: curr["episode_id"],
-              openingText: curr["opening_crawl"],
-              releaseDate: curr["release_date"],
-            });
-            return acc;
-          }, [])
-        );
-      });
+  const fetchMoviesHandler = async (e) => {
+    const data = await (await fetch("https://swapi.dev/api/films/")).json();
+    setMovies(
+      data.results.reduce((acc, curr) => {
+        const { episode_id, opening_crawl, release_date, ...rest } = curr;
+        acc.push({
+          ...rest,
+          id: curr["episode_id"],
+          openingText: curr["opening_crawl"],
+          releaseDate: curr["release_date"],
+        });
+        return acc;
+      }, [])
+    );
   };
 
   return (
