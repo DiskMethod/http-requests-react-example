1b6dcbeee8609ae72afcd05b06dec17218e2d750
Lucas Chagas <lucas.w.chagas@gmail.com>
2022-06-17 22:59:24 -0400

    Added Error Handling

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,34 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const fetchMoviesHandler = async (e) => {
     setIsLoading(true);
-    const data = await (await fetch("https://swapi.dev/api/films/")).json();
-    setMovies(
-      data.results.reduce((acc, curr) => {
-        const { episode_id, opening_crawl, release_date, ...rest } = curr;
-        acc.push({
-          ...rest,
-          id: curr["episode_id"],
-          openingText: curr["opening_crawl"],
-          releaseDate: curr["release_date"],
-        });
-        return acc;
-      }, [])
-    );
+    try {
+      const response = await fetch("https://swapi.dev/api/films/");
+
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+
+      const data = await response.json();
+
+      setMovies(
+        data.results.reduce((acc, curr) => {
+          const { episode_id, opening_crawl, release_date, ...rest } = curr;
+          acc.push({
+            ...rest,
+            id: curr["episode_id"],
+            openingText: curr["opening_crawl"],
+            releaseDate: curr["release_date"],
+          });
+          return acc;
+        }, [])
+      );
+    } catch (error) {
+      setError(error.message);
+    }
     setIsLoading(false);
   };
 
@@ -32,7 +44,8 @@ function App() {
       </section>
       <section>
         {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>Found no movies.</p>}
+        {!isLoading && !error && movies.length === 0 && <p>Found no movies.</p>}
+        {!isLoading && error && <p>{error}</p>}
         {isLoading && <p>Loading...</p>}
       </section>
     </React.Fragment>
